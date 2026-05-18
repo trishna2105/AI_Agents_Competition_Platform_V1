3a72cde20b664d5d75f99b2e574780448a10c965
Trishna <trishna2104@gmail.com>
2026-05-18 10:21:19 +0530

    final fix remove secrets

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:Nord9182finance@db.oyhpuoebrydygxrffidy.supabase.co:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
